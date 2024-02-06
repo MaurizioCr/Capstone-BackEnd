d5e5280542dc7ebd8bf92b5fe352044206fd4e4b
MaurizioCr <crispinomaurizio034@gmail.com>
2024-02-06 17:24:34 +0100

    Exceptions + config

---
+++ src/main/java/capstone/mauriziocrispino/MaurizioCrispino/Exceptions/BadRequestException.java
@@ -0,0 +1,10 @@
+package capstone.mauriziocrispino.MaurizioCrispino.Exceptions;
+
+public class BadRequestException extends RuntimeException{
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+
+
+}
---
+++ src/main/java/capstone/mauriziocrispino/MaurizioCrispino/Exceptions/NotFoundException.java
@@ -0,0 +1,11 @@
+package capstone.mauriziocrispino.MaurizioCrispino.Exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(long id) {
+        super("Elemento con id " + id + " non trovato!");
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
